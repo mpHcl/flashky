0bbfebd4277dc351e0136f3b44c1813f22492dd2
Dave848 <dj300758@student.polsl.pl>
2025-10-09 23:20:36 +0200

    Added relationships, improved file structure
    Co-authored-by: Michał Pilch <mpHcl@users.noreply.github.com>

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -1,19 +1,21 @@
 from sqlalchemy import inspect
 from sqlmodel import create_engine, SQLModel
-from .models.tables import *
+from app.models import *
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
+
 def is_database_initialized():
     return len(inspect(engine).get_table_names()) != 0
 
+
 def init_db():
     if is_database_initialized():
-        print('Database already initialized')
+        print("Database already initialized")
         return
     else:
         SQLModel.metadata.create_all(engine)
+        # populate_database()
         print("Database initialized")
-        
--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,12 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
-from .database import init_db
+from app.database import init_db
 
 app = FastAPI()
+
 
 @app.on_event("startup")
 def startup():
     init_db()
+
 
 @app.get("/")
 def read_root():

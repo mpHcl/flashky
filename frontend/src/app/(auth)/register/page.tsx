a56f6ba8d83843cc8824011d9cff3990cbebac6e
ZofiaLor <zosia984@gmail.com>
2025-10-28 17:21:23 +0100

    Register fetch logic and basic validation

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -11,12 +11,22 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import DrawIcon from '@mui/icons-material/Draw';
-import FormControl from '@mui/material/FormControl';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
+import { useRouter } from "next/navigation";
+
+function CustomHelperText() {
+  const { error } = useFormControl() || {};
+  return (
+    <FormHelperText error={error}>
+      {error ? "This field is required" : ""}
+    </FormHelperText>
+  );
+}
 
 export default function Register() {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -31,15 +41,64 @@ export default function Register() {
     event.preventDefault();
   };
 
+  const [errorUsername, setErrorUsername] = React.useState(false);
+  const [errorEmail, setErrorEmail] = React.useState(false);
+  const [errorPassword, setErrorPassword] = React.useState(false);
+
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const router = useRouter();
+
+  const onRegisterButtonClick = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": username,
+      "email": email,
+      "password": password
+    });
+
+    console.log(raw);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://127.0.0.1:8000/register", requestOptions)
+      .then((response) => {
+        console.log(response)
+        return response.json()
+      })
+      .then((result) => {
+        if (result['token']) {
+          localStorage.setItem("token", result['token']);
+          router.push("/");
+        }
+        else {
+          alert(result.detail)
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
       <Paper elevation={1} sx={{ minWidth: 400, maxWidth: 600 }}>
         <Stack spacing={2} justifyContent="center" alignItems="center" p={2} sx={{ margin: 2 }}>
           <Typography variant="h4" gutterBottom>Welcome to Flashky!</Typography>
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required error={errorUsername}>
             <InputLabel htmlFor="register-username-input">Username</InputLabel>
             <OutlinedInput
               id="register-username-input"
+              onChange={(e) => {
+                setUsername(e.target.value);
+                setErrorUsername(e.target.value.length < 1);
+              }}
               endAdornment={
                 <InputAdornment position="end">
                   <PersonIcon />
@@ -47,11 +106,16 @@ export default function Register() {
               }
               label="Username"
             />
+            <CustomHelperText />
           </FormControl>
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required error={errorEmail}>
             <InputLabel htmlFor="register-email-input">E-mail</InputLabel>
             <OutlinedInput
               id="register-email-input"
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setErrorEmail(e.target.value.length < 1);
+              }}
               endAdornment={
                 <InputAdornment position="end">
                   <AlternateEmailIcon />
@@ -59,11 +123,16 @@ export default function Register() {
               }
               label="E-mail"
             />
+            <CustomHelperText />
           </FormControl>
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required error={errorPassword}>
             <InputLabel htmlFor="register-password-input">Password</InputLabel>
             <OutlinedInput
               id="register-password-input"
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setErrorPassword(e.target.value.length < 1);
+              }}
               type={showPassword ? 'text' : 'password'}
               endAdornment={
                 <InputAdornment position="end">
@@ -82,6 +151,7 @@ export default function Register() {
               }
               label="Password"
             />
+            <CustomHelperText />
           </FormControl>
           <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
             <InputLabel htmlFor="register-description-input">Description</InputLabel>
@@ -97,7 +167,7 @@ export default function Register() {
             />
             <FormHelperText>Tell us a little about yourself</FormHelperText>
           </FormControl>
-          <Button color="secondary" size="large" variant="contained">Register</Button>
+          <Button color="secondary" size="large" variant="contained" onClick={onRegisterButtonClick} disabled={username.length == 0 || email.length == 0 || password.length == 0}>Register</Button>
           <Typography variant="caption" gutterBottom>Already have an account? <Link href="/login">Log in!</Link></Typography>
         </Stack>
       </Paper>

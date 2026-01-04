97535469ab78056fd17e61db68ade4e88f670322
ZofiaLor <zosia984@gmail.com>
2026-01-04 21:51:26 +0100

    change route and imports

--- frontend/src/app/mydecks/page.tsx
+++ frontend/src/app/mydecks/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 import AddIcon from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import { Suspense, useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, Button, Link, Typography } from '@mui/material';
 import CrudList from '../components/crudlist';
 import { fetchAuthGET } from '../lib/fetch';
 
@@ -20,7 +17,7 @@ export default function MyDecks() {
     }, []);
     return (<>
         <Typography variant="h2" gutterBottom>My Decks</Typography>
-        <Link href="/mydecks/newdeck"><Button color="secondary" size="large" variant="outlined"><AddIcon /> Create a new deck</Button></Link>
+        <Link href="/mydecks/add"><Button color="secondary" size="large" variant="outlined"><AddIcon /> Create a new deck</Button></Link>
         <Box>
             <Suspense fallback={<div>Loading...</div>}>
                 <CrudList data={data.map((el) => ({ id: el.id, name: el.name, preview: el.description }))} showUpdateDeleteBtns={true} path="deck"></CrudList>

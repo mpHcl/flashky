41e15b1dea0a95ea76d5c0e7780ed65b92d482dd
Dave848 <dj300758@student.polsl.pl>
2026-01-23 21:22:13 +0100

    refactorization

--- frontend/src/app/(moderation)/users/lib/functions.tsx
+++ frontend/src/app/(moderation)/users/lib/functions.tsx
@@ -2,7 +2,7 @@ import { Chip } from "@mui/material";
 import { User } from "./types";
 
 export const userCardHeaderAction = (user: User) => {
-  let label = user.active ? "Active" : "Inactive";
-  let color: "error" | "success" | "warning" = user.active ? "success" : "error";
+  const label = user.active ? "Active" : "Inactive";
+  const color: "error" | "success" = user.active ? "success" : "error";
   return (<Chip sx={{ width: 150 }} label={label} color={color} />)
 };
